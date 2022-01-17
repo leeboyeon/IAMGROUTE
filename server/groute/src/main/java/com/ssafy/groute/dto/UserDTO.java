792b4cab9419476f8c586a59048e747e49e8bed8
pdj1649 <pdj1649@gmail.com>
2022-01-17 16:21:59 +0900

    complete user dto

--- server/groute/src/main/java/com/ssafy/groute/dto/UserDTO.java
+++ server/groute/src/main/java/com/ssafy/groute/dto/UserDTO.java
@@ -1,4 +1,41 @@
 package com.ssafy.groute.dto;
 
+import com.sun.istack.internal.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDTO {
+
+    @NotNull
+    private String id;
+
+    @NotNull
+    private String password;
+
+    private String nickname;
+
+    private String phone;
+
+    private String gender;
+
+    private String birth;
+
+    private String email;
+
+    @NotNull
+    private String type;
+
+    private String token;
+
+    private String img;
+
+    private String createDate;
+
+    private String updateDate;
 }
