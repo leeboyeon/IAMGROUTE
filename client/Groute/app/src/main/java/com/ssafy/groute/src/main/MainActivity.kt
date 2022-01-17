7042429199b9255d15dbcbcb96c7c71acf776f58
LeeBoyeon <61018094+leeboyeon@users.noreply.github.com>
2022-01-17 15:52:11 +0900

    Added BottomNav and ProfileBar

--- client/Groute/app/src/main/java/com/ssafy/groute/src/main/MainActivity.kt
+++ client/Groute/app/src/main/java/com/ssafy/groute/src/main/MainActivity.kt
@@ -3,10 +3,13 @@ package com.ssafy.groute.src.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ssafy.groute.R
+import com.ssafy.groute.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
