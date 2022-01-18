8f69b4bccf25f758c20e2f111acf6068b980d050
LeeBoyeon <61018094+leeboyeon@users.noreply.github.com>
2022-01-18 16:09:24 +0900

    error

--- client/Groute/app/src/main/java/com/ssafy/groute/src/main/my/MyFragment.kt
+++ client/Groute/app/src/main/java/com/ssafy/groute/src/main/my/MyFragment.kt
@@ -8,27 +8,18 @@ import android.view.View
 import android.view.ViewGroup
 import com.google.android.material.tabs.TabLayoutMediator
 import com.ssafy.groute.R
-<<<<<<< HEAD
-=======
 import com.ssafy.groute.databinding.FragmentMyBinding
->>>>>>> feature/MyFragment_Frontend
 import com.ssafy.groute.src.main.MainActivity
 
 
 class MyFragment : Fragment() {
-<<<<<<< HEAD
-    private lateinit var mainActivity: MainActivity
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        mainActivity.hideMainProfileBar(true)
-
-=======
     private lateinit var binding: FragmentMyBinding
     private lateinit var mainActivity:MainActivity
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
->>>>>>> feature/MyFragment_Frontend
+        mainActivity.hideMainProfileBar(true)
     }
 
     override fun onAttach(context: Context) {
@@ -43,9 +34,6 @@ class MyFragment : Fragment() {
         binding = FragmentMyBinding.inflate(inflater,container,false)
         return binding.root
     }
-
-<<<<<<< HEAD
-=======
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val pagerAdapter = MyTravelTabPageAdapter(this)
@@ -60,13 +48,6 @@ class MyFragment : Fragment() {
             tab.text = tabList.get(position)
         }.attach()
     }
-    companion object {
 
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MyFragment().apply {
 
-            }
-    }
->>>>>>> feature/MyFragment_Frontend
 }
