b2c43e292d15fc62625556c5ff715595c9bf1e0f
송경희 <kyunghee794613@gmail.com>
2022-01-18 13:27:07 +0900

    Create Board Recyclerview item

--- client/Groute/app/src/main/java/com/ssafy/groute/src/main/board/BoardFragment.kt
+++ client/Groute/app/src/main/java/com/ssafy/groute/src/main/board/BoardFragment.kt
@@ -6,15 +6,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.ssafy.groute.R
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ssafy.groute.databinding.FragmentBoardBinding
-import com.ssafy.groute.databinding.FragmentHomeBinding
 import com.ssafy.groute.src.main.MainActivity
 
 
 class BoardFragment : Fragment() {
     lateinit var binding: FragmentBoardBinding
     private lateinit var mainActivity: MainActivity
+    lateinit var boardRecyclerviewAdapter: BoardRecyclerviewAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainActivity.hideMainProfileBar(true)
@@ -33,4 +34,15 @@ class BoardFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        boardRecyclerviewAdapter = BoardRecyclerviewAdapter()
+
+        binding.boardRv.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = boardRecyclerviewAdapter
+
+        }
+    }
 }
---
+++ client/Groute/app/src/main/java/com/ssafy/groute/src/main/board/BoardRecyclerviewAdapter.kt
@@ -0,0 +1,49 @@
+package com.ssafy.groute.src.main.board
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.ssafy.groute.R
+import de.hdodenhof.circleimageview.CircleImageView
+
+class BoardRecyclerviewAdapter() : RecyclerView.Adapter<BoardRecyclerviewAdapter.BoardHolder>(){
+
+    inner class BoardHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val profileIv = itemView.findViewById<CircleImageView>(R.id.item_board_profile_iv)
+        val uidTv = itemView.findViewById<TextView>(R.id.item_board_uid_tv)
+        val thumbnailIv = itemView.findViewById<ImageView>(R.id.item_board_iv)
+        val titleTv = itemView.findViewById<TextView>(R.id.item_board_title_tv)
+        val contentTv = itemView.findViewById<TextView>(R.id.item_board_content_tv)
+        val reviewTv = itemView.findViewById<TextView>(R.id.item_board_review_num_tv)
+        val likeTv = itemView.findViewById<TextView>(R.id.item_board_like_num_tv)
+        val themeRv = itemView.findViewById<RecyclerView>(R.id.item_board_theme_rv)
+
+        fun bindInfo() {
+            profileIv.setBackgroundResource(R.drawable.profile)
+            uidTv.text = "Ponyo"
+            thumbnailIv.setBackgroundResource(R.drawable.flower)
+            titleTv.text = "유채꽃밭이 너무 아름다웠던 제주~!!!"
+            contentTv.text = "제주도 성산유채꽃밭이 유명하다는 얘기를 듣고 다녀왔습니다. 너무 아름다웠고 나중에 기회가 된다면 또 가고싶네요! 연인과의 유채꽃밭 데이트 추천합니다ㅎㅎ"
+            reviewTv.text = "22"
+            likeTv.text = "56"
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BoardHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item_board, parent, false)
+        return BoardHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: BoardHolder, position: Int) {
+        holder.apply {
+            bindInfo()
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return 6
+    }
+}
