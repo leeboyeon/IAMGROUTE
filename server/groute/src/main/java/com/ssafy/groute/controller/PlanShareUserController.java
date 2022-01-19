2ee18475257e05e7695e8a8e0955d079314b200e
dohyung102 <87457598+dohyung102@users.noreply.github.com>
2022-01-19 15:07:58 +0900

    backend_planshareuser_complete

---
+++ server/groute/src/main/java/com/ssafy/groute/controller/PlanShareUserController.java
@@ -0,0 +1,87 @@
+package com.ssafy.groute.controller;
+
+import com.ssafy.groute.dto.PlanShareUser;
+import com.ssafy.groute.service.PlanShareUserService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/planShareUser")
+@CrossOrigin(origins = { "*" }, methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT,
+        RequestMethod.DELETE }, maxAge = 6000)
+public class PlanShareUserController {
+
+    @Autowired
+    PlanShareUserService planShareUserService;
+
+    @ApiOperation(value = "planShareUser 추가",notes = "planShareUser 추가")
+    @PostMapping(value = "/insert")
+    public ResponseEntity<?> insertPlanShareUser(@RequestBody PlanShareUser req){
+
+        try {
+            planShareUserService.insertPlanShareUser(req);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<String>("FAIL", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        return new ResponseEntity<String>("SUCCESS",HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "planShareUser 검색",notes = "이름으로 planShareUser 하나 검색")
+    @GetMapping(value = "/detail")
+    public ResponseEntity<?> detailPlanShareUser(@RequestParam("id") int id) throws Exception{
+
+        PlanShareUser res = planShareUserService.selectPlanShareUser(id);
+        if(res==null){
+            return new ResponseEntity<String>("FAIL", HttpStatus.NO_CONTENT);
+        }
+
+        return new ResponseEntity<PlanShareUser>(res,HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "list planShareUser",notes = "모든 planShareUser 반환")
+    @GetMapping(value = "/list")
+    public ResponseEntity<?> listPlanShareUser() throws Exception{
+
+        List<PlanShareUser> res = planShareUserService.selectAllPlanShareUser();
+        if(res==null){
+            return new ResponseEntity<String>("FAIL", HttpStatus.NO_CONTENT);
+        }
+
+        return new ResponseEntity<List<PlanShareUser>>(res,HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "delete planShareUser",notes = "planShareUser 삭제")
+    @DeleteMapping(value = "/del")
+    public ResponseEntity<?> deletePlanShareUser(@RequestParam("id") int id) throws Exception{
+
+        try {
+            planShareUserService.deletePlanShareUser(id);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<String>("FAIL", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        return new ResponseEntity<String>("SUCCESS",HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "updatePlanShareUser",notes = "planShareUser 수정")
+    @PutMapping(value = "/update")
+    public ResponseEntity<?> updatePlanShareUser(@RequestBody PlanShareUser planShareUser) throws Exception{
+
+        try {
+            planShareUserService.updatePlanShareUser(planShareUser);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<String>("FAIL", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        return new ResponseEntity<String>("SUCCESS",HttpStatus.OK);
+    }
+}
---
+++ server/groute/src/main/java/com/ssafy/groute/dto/PlanShareUser.java
@@ -0,0 +1,17 @@
+package com.ssafy.groute.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlanShareUser {
+    private int id;
+    private String userId;
+    private int planId;
+
+}
