84248f2483eef1e8fd51617cf73d883fb8be8738
pdj1649 <pdj1649@gmail.com>
2022-01-19 13:48:49 +0900

    #S06P12D109-90 complete BE account CRUD

---
+++ server/groute/src/main/java/com/ssafy/groute/service/AccountServiceImpl.java
@@ -0,0 +1,39 @@
+package com.ssafy.groute.service;
+
+import com.ssafy.groute.dto.Account;
+import com.ssafy.groute.mapper.AccountMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AccountServiceImpl implements AccountService {
+    @Autowired
+    AccountMapper accountMapper;
+
+    @Override
+    public void insertAccount(Account account) throws Exception {
+        accountMapper.insertAccount(account);
+    }
+
+    @Override
+    public Account selectAccount(int id) throws Exception {
+        return accountMapper.selectAccount(id);
+    }
+
+    @Override
+    public List<Account> selectAllAccount() throws Exception {
+        return accountMapper.selectAllAccount();
+    }
+
+    @Override
+    public void deleteAccount(int id) throws Exception {
+        accountMapper.deleteAccount(id);
+    }
+
+    @Override
+    public void updateAccount(Account account) throws Exception {
+        accountMapper.updateAccount(account);
+    }
+}
