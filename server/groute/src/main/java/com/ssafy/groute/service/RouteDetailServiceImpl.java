200655b7b15374ebeb460d12d83029def76be017
dohyung102 <87457598+dohyung102@users.noreply.github.com>
2022-01-19 14:26:42 +0900

    backend_routedetail_complete

---
+++ server/groute/src/main/java/com/ssafy/groute/service/RouteDetailServiceImpl.java
@@ -0,0 +1,40 @@
+package com.ssafy.groute.service;
+
+import com.ssafy.groute.dto.RouteDetail;
+import com.ssafy.groute.mapper.RouteDetailMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RouteDetailServiceImpl implements RouteDetailService{
+
+    @Autowired
+    RouteDetailMapper routeDetailMapper;
+
+    @Override
+    public void insertRouteDetail(RouteDetail routeDetail) throws Exception {
+        routeDetailMapper.insertRouteDetail(routeDetail);
+    }
+
+    @Override
+    public RouteDetail selectRouteDetail(int id) throws Exception {
+        return routeDetailMapper.selectRouteDetail(id);
+    }
+
+    @Override
+    public List<RouteDetail> selectAllRouteDetail() throws Exception {
+        return routeDetailMapper.selectAllRouteDetail();
+    }
+
+    @Override
+    public void deleteRouteDetail(int id) throws Exception {
+        routeDetailMapper.deleteRouteDetail(id);
+    }
+
+    @Override
+    public void updateRouteDetail(RouteDetail routeDetail) throws Exception {
+        routeDetailMapper.updateRouteDetail(routeDetail);
+    }
+}
