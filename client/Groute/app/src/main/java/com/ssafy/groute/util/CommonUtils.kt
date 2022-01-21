c45bcbdd0c5e555d7b5699c21322eb50e577268f
jiwoo <63338678+dino889@users.noreply.github.com>
2022-01-21 17:23:17 +0900

    #S06P12D109-14 / complete - sns Login Naver

--- client/Groute/app/src/main/java/com/ssafy/groute/util/CommonUtils.kt
+++ client/Groute/app/src/main/java/com/ssafy/groute/util/CommonUtils.kt
@@ -1,82 +1,76 @@
-package com.ssafy.groute.util
-
-import android.icu.text.StringPrepParseException
-import android.util.Log
-import android.widget.TextView
-import com.ssafy.cafe.R
-import com.ssafy.cafe.config.ApplicationClass
-import com.ssafy.cafe.src.main.network.response.LatestOrderResponse
-import com.ssafy.cafe.src.main.network.response.OrderDetailResponse
-import com.ssafy.groute.config.ApplicationClass
-import java.text.DecimalFormat
-import java.text.SimpleDateFormat
-import java.util.*
-
-object CommonUtils {
-
-    //천단위 콤마
-    fun makeComma(num: Int): String {
-        var comma = DecimalFormat("#,###")
-        return "${comma.format(num)} 원"
-    }
-
-    fun getFormattedString(date:Date): String {
-        val dateFormat = SimpleDateFormat("yyyy.MM.dd HH시 mm분 ss초")
-        dateFormat.timeZone = TimeZone.getTimeZone("Seoul/Asia")
-
-        return dateFormat.format(date)
-    }
-
-    fun getFormattedStringDate(date:Date): String {
-        val dateFormat = SimpleDateFormat("yyyy.MM.dd")
-        Log.d("싸피", "getFormattedStringDate: $dateFormat")
-        dateFormat.timeZone = TimeZone.getTimeZone("Seoul/Asia")
-
-        return dateFormat.format(date)
-    }
-
-    // 시간 계산을 통해 완성된 제품인지 확인
-    fun isOrderCompleted(orderDetail: OrderDetailResponse): String {
-        return if( checkTime(orderDetail.orderDate.time))  "주문완료" else "진행 중.."
-    }
-
-    // 시간 계산을 통해 완성된 제품인지 확인
-    fun isOrderCompleted(order: LatestOrderResponse): String {
-        return if( checkTime(order.orderDate.time))  "주문완료" else "진행 중.."
-    }
-    fun dialogProductComent(pName: String) : String {
-        return "주문하신 ${pName} 어떠셨나요?"
-    }
-    private fun checkTime(time:Long):Boolean{
-        val curTime = (Date().time+60*60*9*1000)
-
-        return (curTime - time) > ApplicationClass.ORDER_COMPLETED_TIME
-    }
-    fun convertOptionMenu(type:Int?, syrup:String?, shot:Int?) : String{
-        val types = if(type == 0){
-            "HOT"
-        }else if(type == 1){
-            "ICE"
-        } else if(type == 3){
-            "없음"
-        } else {
-            ""
-        }
-
-        val syrups = if(syrup == null || syrup.equals("null") || syrup.equals("")){
-            "없음"
-        }else if(syrup.equals("설탕")){
-            "${syrup}(+0원)"
-        }else{
-            "${syrup}(+500원)"
-        }
-
-        val shots = if(shot == 0 || shot.toString().equals("null") || shot == null){
-            "없음"
-        }else{
-            "${shot}개 추가"
-        }
-
-        return "$types | $syrups | 샷(+500원) $shots"
-    }
-}
+//package com.ssafy.groute.util
+//
+//import android.util.Log
+//import com.ssafy.groute.config.ApplicationClass
+//import java.text.DecimalFormat
+//import java.text.SimpleDateFormat
+//import java.util.*
+//
+//object CommonUtils {
+//
+//    //천단위 콤마
+//    fun makeComma(num: Int): String {
+//        var comma = DecimalFormat("#,###")
+//        return "${comma.format(num)} 원"
+//    }
+//
+//    fun getFormattedString(date:Date): String {
+//        val dateFormat = SimpleDateFormat("yyyy.MM.dd HH시 mm분 ss초")
+//        dateFormat.timeZone = TimeZone.getTimeZone("Seoul/Asia")
+//
+//        return dateFormat.format(date)
+//    }
+//
+//    fun getFormattedStringDate(date:Date): String {
+//        val dateFormat = SimpleDateFormat("yyyy.MM.dd")
+//        Log.d("싸피", "getFormattedStringDate: $dateFormat")
+//        dateFormat.timeZone = TimeZone.getTimeZone("Seoul/Asia")
+//
+//        return dateFormat.format(date)
+//    }
+//
+//    // 시간 계산을 통해 완성된 제품인지 확인
+//    fun isOrderCompleted(orderDetail: OrderDetailResponse): String {
+//        return if( checkTime(orderDetail.orderDate.time))  "주문완료" else "진행 중.."
+//    }
+//
+//    // 시간 계산을 통해 완성된 제품인지 확인
+//    fun isOrderCompleted(order: LatestOrderResponse): String {
+//        return if( checkTime(order.orderDate.time))  "주문완료" else "진행 중.."
+//    }
+//    fun dialogProductComent(pName: String) : String {
+//        return "주문하신 ${pName} 어떠셨나요?"
+//    }
+//    private fun checkTime(time:Long):Boolean{
+//        val curTime = (Date().time+60*60*9*1000)
+//
+//        return (curTime - time) > ApplicationClass.ORDER_COMPLETED_TIME
+//    }
+//    fun convertOptionMenu(type:Int?, syrup:String?, shot:Int?) : String{
+//        val types = if(type == 0){
+//            "HOT"
+//        }else if(type == 1){
+//            "ICE"
+//        } else if(type == 3){
+//            "없음"
+//        } else {
+//            ""
+//        }
+//
+//        val syrups = if(syrup == null || syrup.equals("null") || syrup.equals("")){
+//            "없음"
+//        }else if(syrup.equals("설탕")){
+//            "${syrup}(+0원)"
+//        }else{
+//            "${syrup}(+500원)"
+//        }
+//
+//        val shots = if(shot == 0 || shot.toString().equals("null") || shot == null){
+//            "없음"
+//        }else{
+//            "${shot}개 추가"
+//        }
+//
+//        return "$types | $syrups | 샷(+500원) $shots"
+//    }
+//}
