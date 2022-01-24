7d61ee938b0045ad7e29dc30e45683501b27dec4
LeeBoyeon <61018094+leeboyeon@users.noreply.github.com>
2022-01-24 17:10:15 +0900

    Update the write Board

--- client/Groute/app/src/main/java/com/ssafy/groute/src/api/BoardApi.kt
+++ client/Groute/app/src/main/java/com/ssafy/groute/src/api/BoardApi.kt
@@ -4,11 +4,10 @@ import com.ssafy.groute.src.dto.BoardDetail
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
-<<<<<<< HEAD
+
 import retrofit2.http.Query
-=======
+
 import retrofit2.http.POST
->>>>>>> feature/BoardF_create
 
 interface BoardApi {
 
@@ -16,12 +15,11 @@ interface BoardApi {
     @GET("/boardDetail/list")
     fun listBoard() : Call<MutableList<BoardDetail>>
 
-<<<<<<< HEAD
     // 게시판 타입에 따른 리스트 조회
     @GET("/boardDetail/list/division")
     fun listBoardDetail(@Query("boardId") boardId : Int) : Call<MutableList<BoardDetail>>
-=======
+
     @POST("/boardDetail/insert")
     fun insertBoardDetail(@Body boardDetail:BoardDetail) : Call<Boolean>
->>>>>>> feature/BoardF_create
+
 }
