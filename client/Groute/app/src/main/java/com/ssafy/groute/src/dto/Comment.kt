c7c3a8e4dacf0a404f709eca6fe92cb13097156d
송경희 <kyunghee794613@gmail.com>
2022-01-26 11:47:28 +0900

    Comment DTO Create

---
+++ client/Groute/app/src/main/java/com/ssafy/groute/src/dto/Comment.kt
@@ -0,0 +1,13 @@
+package com.ssafy.groute.src.dto
+
+data class Comment(
+    val id: Int,
+    val boardDetailId: Int,
+    val content: String,
+    val groupNum: Int,
+    val level: Int,
+    val order: Int,
+    val userId: String
+) {
+    constructor(boardDetailId: Int, content: String, groupNum: Int, userId: String): this(0, boardDetailId, content, groupNum, 0, 0, userId)
+}
