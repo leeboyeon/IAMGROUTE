07b71b35e68717cc454a7c8248563c5726028df0
LeeBoyeon <61018094+leeboyeon@users.noreply.github.com>
2022-02-01 14:24:42 +0900

    Complete the Magazine F Success

--- client/Groute/app/src/main/java/com/ssafy/groute/src/main/board/MagazineAdapter.kt
+++ client/Groute/app/src/main/java/com/ssafy/groute/src/main/board/MagazineAdapter.kt
@@ -6,6 +6,10 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.MultiTransformation
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 import com.ssafy.groute.R
 import com.ssafy.groute.src.dto.Magazine
 
@@ -14,8 +18,10 @@ class MagazineAdapter : RecyclerView.Adapter<MagazineAdapter.MagazineHolder>(){
     var list = mutableListOf<Magazine>()
     inner class MagazineHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         fun bindInfo(data : Magazine){
+            val option = MultiTransformation(CenterCrop(),RoundedCorners(30))
             Glide.with(itemView)
                 .load(data.img)
+                .apply(RequestOptions.bitmapTransform(option))
                 .into(itemView.findViewById(R.id.magazine_iv_img))
 
             itemView.findViewById<TextView>(R.id.magazine_tv_title).text = data.title
@@ -31,9 +37,9 @@ class MagazineAdapter : RecyclerView.Adapter<MagazineAdapter.MagazineHolder>(){
     override fun onBindViewHolder(holder: MagazineHolder, position: Int) {
         holder.apply {
             bindInfo(list[position])
-            itemView.setOnClickListener {
-                itemClickListener.onClick(it, position, list[position].title)
-            }
+//            itemView.setOnClickListener {
+//                itemClickListener.onClick(it, position, list[position].title)
+//            }
         }
     }
 
