c98def9bf0b8d1731c59f05050c5eb7cf3b4f488
송경희 <kyunghee794613@gmail.com>
2022-02-04 10:32:02 +0900

    Update MainViewModel

--- client/Groute/app/src/main/java/com/ssafy/groute/src/viewmodel/MainViewModel.kt
+++ client/Groute/app/src/main/java/com/ssafy/groute/src/viewmodel/MainViewModel.kt
@@ -9,6 +9,18 @@ import com.ssafy.groute.src.service.UserService
 
 open class MainViewModel : ViewModel() {
     val userInfo = UserService().getUserInfo(ApplicationClass.sharedPreferencesUtil.getUser().id)
+    private val _bannerItemList: MutableLiveData<List<Int>> = MutableLiveData()
+    private val _currentPosition: MutableLiveData<Int> = MutableLiveData()
+
+    val bannerItemList: LiveData<List<Int>>
+        get() = _bannerItemList
+
+    val currentPosition: LiveData<Int>
+        get() = _currentPosition
+
+    init{
+        _currentPosition.value = 0
+    }
 
     private var users = MutableLiveData<LiveData<UserInfoResponse>>().apply {
         value = userInfo
@@ -17,5 +29,16 @@ open class MainViewModel : ViewModel() {
     fun getUser() :LiveData<UserInfoResponse>{
         return userInfo
     }
+
+    fun setBannerItems(list: List<Int>) {
+        _bannerItemList.value = list
+    }
+
+    fun setCurrentPosition(position: Int) {
+        _currentPosition.value = position
+    }
+
+
+    fun getcurrentPosition() = currentPosition.value
 
 }
