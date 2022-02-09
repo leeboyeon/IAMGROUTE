3a735a904df08043ce9df63f81e73c5fa460e0bc
LeeBoyeon <61018094+leeboyeon@users.noreply.github.com>
2022-02-09 17:52:18 +0900

    Complete memo delete modify

--- client/Groute/app/src/main/java/com/ssafy/groute/src/main/travel/TravelPlanFragment.kt
+++ client/Groute/app/src/main/java/com/ssafy/groute/src/main/travel/TravelPlanFragment.kt
@@ -3,76 +3,46 @@ package com.ssafy.groute.src.main.travel
 import android.animation.ValueAnimator
 import android.annotation.SuppressLint
 import android.app.AlertDialog
-import android.app.DatePickerDialog
-import android.app.DatePickerDialog.OnDateSetListener
 import android.app.Dialog
 import android.content.Context
-import android.content.DialogInterface
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.*
-import androidx.fragment.app.Fragment
-import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.tabs.TabLayoutMediator
 import com.ssafy.groute.R
 import com.ssafy.groute.config.BaseFragment
-import com.ssafy.groute.databinding.FragmentBoardBinding
 import com.ssafy.groute.databinding.FragmentTravelPlanBinding
 import com.ssafy.groute.src.main.MainActivity
-import com.ssafy.groute.src.main.my.MyTravelFragment
-import com.ssafy.groute.src.main.route.RouteTabPageAdapter
 import com.ssafy.groute.src.viewmodel.PlanViewModel
 import kotlinx.coroutines.runBlocking
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
-import android.view.View.inflate
 import android.widget.*
-import androidx.databinding.DataBindingUtil.inflate
-import androidx.fragment.app.FragmentManager
+import androidx.appcompat.widget.AppCompatButton
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.gson.GsonBuilder
-import com.ssafy.groute.databinding.ActivityCommentNestedBinding.inflate
-import java.lang.Integer.max
-import java.text.ParseException
-import java.text.SimpleDateFormat
-import java.time.LocalDateTime
 import java.util.*
-import java.util.Date.from
-import java.util.concurrent.TimeUnit
-import kotlin.collections.ArrayList
-import android.widget.DatePicker
-import androidx.appcompat.widget.AppCompatButton
-import androidx.core.view.isVisible
-import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.ItemTouchHelper
 import com.google.android.material.datepicker.*
 import com.google.android.material.tabs.TabLayout
 import com.ssafy.groute.config.ApplicationClass
-import com.ssafy.groute.databinding.ActivityCommentNestedBinding.bind
-import com.ssafy.groute.src.dto.Place
 import com.ssafy.groute.src.dto.Route
 import com.ssafy.groute.src.dto.RouteDetail
+import com.ssafy.groute.src.dto.UserPlan
 import com.ssafy.groute.src.service.RouteDetailService
 import com.ssafy.groute.src.service.RouteService
 import com.ssafy.groute.src.service.UserPlanService
 import com.ssafy.groute.src.viewmodel.HomeViewModel
 import com.ssafy.groute.src.viewmodel.PlaceViewModel
 import com.ssafy.groute.util.RetrofitCallback
-import kotlinx.coroutines.selects.select
-import net.daum.mf.map.api.MapPOIItem
 import net.daum.mf.map.api.MapPoint
 import net.daum.mf.map.api.MapView
-import kotlin.time.days
 
 
 private const val TAG = "TravelPlanFragment"
@@ -176,30 +146,71 @@ class TravelPlanFragment : BaseFragment<FragmentTravelPlanBinding>(FragmentTrave
     }
 
     fun initMemo(){
-            planViewModel.routeList.observe(viewLifecycleOwner, {
-                if(it[curPos].memo != null){
-                    val dialogView = LayoutInflater.from(requireContext()).inflate(R.layout.dialog_show_memo,null)
-                    val dialog = Dialog(requireContext())
-                    if(dialogView.parent != null){
-                        (dialogView.parent as ViewGroup).removeView(dialogView)
-                    }
-                    dialog.setContentView(dialogView)
-                    dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        var routes = planViewModel.routeList.value!!
+        if(routes[curPos].memo != null){
+            val dialogView = LayoutInflater.from(requireContext()).inflate(R.layout.dialog_show_memo,null)
+            val dialog = Dialog(requireContext())
+            if(dialogView.parent != null){
+                (dialogView.parent as ViewGroup).removeView(dialogView)
+            }
+            dialog.setContentView(dialogView)
+            dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            dialogView.findViewById<TextView>(R.id.dayMemo).text = routes[curPos].memo
+            dialog.show()
 
-                    dialogView.findViewById<ImageButton>(R.id.memo_cancle).setOnClickListener {
-                        dialog.dismiss()
-                    }
-                    Log.d(TAG, "initMemo: day")
-                    dialogView.findViewById<TextView>(R.id.dayMemo).text = it[curPos].memo
-                    dialog.show()
-                }else {
-                    showCustomToast("메모가 없습니다.")
-                }
-            })
+            dialogView.findViewById<ImageButton>(R.id.memo_cancle).setOnClickListener {
+                dialog.dismiss()
+            }
 
+            dialogView.findViewById<AppCompatButton>(R.id.showmemo_btn_modify).setOnClickListener {
+                dialog.dismiss()
+                dialog.cancel()
+                insertMemo(0,2)
+            }
 
+            dialogView.findViewById<AppCompatButton>(R.id.showmemo_btn_delete).setOnClickListener {
+                var route = Route(
+                    day = routes[curPos].day,
+                    id = routes[curPos].id,
+                    isCustom = routes[curPos].isCustom,
+                    memo = "",
+                    name = routes[curPos].name,
+                    routeDetailList = routes[curPos].routeDetailList,
+                )
+                updateMemo(route,3)
+                dialog.dismiss()
+            }
+        }
 
     }
+    fun updateMemo(route:Route,flag:Int){
+        //flag 1 : insert/ 2:modify 3:delete
+        RouteService().updateRoute(route, object:RetrofitCallback<Boolean> {
+            override fun onError(t: Throwable) {
+                Log.d(TAG, "onError: ")
+            }
+            override fun onSuccess(code: Int, responseData: Boolean) {
+                Log.d(TAG, "onSuccess: ")
+                if(flag == 1){
+                    showCustomToast("추가되었습니다")
+                }
+                if(flag == 2){
+                    showCustomToast("수정되었습니다")
+                }
+                if(flag == 3){
+                    showCustomToast("삭제되었습니다.")
+                }
+                runBlocking {
+                    planViewModel.getPlanById(planId)
+                }
+            }
+
+            override fun onFailure(code: Int) {
+                Log.d(TAG, "onFailure: ")
+            }
+        })
+    }
+
     fun initKakaoMap(){
         var mapView = MapView(requireContext())
         binding.travelplanMapview.addView(mapView)
@@ -236,7 +247,7 @@ class TravelPlanFragment : BaseFragment<FragmentTravelPlanBinding>(FragmentTrave
         memoAddButton.setOnClickListener{
             fbAnim()
             var placeId = 0
-            insertMemo(placeId)
+            insertMemo(placeId,1)
         }
         routeRecomButton.setOnClickListener {
             fbAnim()
@@ -316,26 +327,25 @@ class TravelPlanFragment : BaseFragment<FragmentTravelPlanBinding>(FragmentTrave
             travelPlanListRecyclerviewAdapter.setMemoClickListener(object:TravelPlanListRecyclerviewAdapter.MemoClickListener{
                 override fun onClick(view: View, position: Int, placeId: Int) {
                     Log.d(TAG, "onClick: memo button Click")
-
                     var memo = ""
-                    planViewModel.routeList.observe(viewLifecycleOwner, {
-                        val details = it[curPos].routeDetailList
+                    var routes = planViewModel.routeList.value!!
+                    val details = routes[curPos].routeDetailList
 
-                        Log.d(TAG, "onClick: ${details}")
-                        for(i in 0.. details.size-1){
-                            Log.d(TAG, "onClick: ${placeId} ||${details[i].placeId}")
-                            if (details[i].placeId == placeId) {
-                                Log.d(TAG, "onClick: findPlace")
-                                Log.d(TAG, "onClick_MEMO: ${details[i].memo}")
-                                memo = details[i].memo
-                                break;
-                            }
+                    for(i in 0.. details.size-1){
+                        if (details[i].placeId == placeId) {
+                            Log.d(TAG, "onClick: findPlace")
+                            Log.d(TAG, "onClick_MEMO: ${details[i].memo}")
+                            memo = details[i].memo
+                            break;
                         }
-                    })
+                    }
 
                     if(memo.equals("")|| memo.isEmpty() || memo.length == 0){
                         //메모가 없으면
-                        insertMemo(placeId)
+                        insertMemo(placeId,1)
+                    }
+                    if(!memo.isEmpty()){
+                        insertMemo(placeId, 2)
                     }
 
                 }
@@ -357,38 +367,72 @@ class TravelPlanFragment : BaseFragment<FragmentTravelPlanBinding>(FragmentTrave
         })
 
     }
-    fun insertMemo(placeId:Int){
-        Log.d(TAG, "insertMemo: ${placeId}")
+    fun insertMemo(placeId:Int , flag : Int){
+        Log.d(TAG, "insertMemo: ${flag}")
+        // flag 1 -> insert 2-> modify
+
         val dialogView = LayoutInflater.from(requireContext()).inflate(R.layout.dialog_insert_memo,null)
+        if(dialogView.parent!=null){
+            (dialogView.parent as ViewGroup).removeAllViews()
+        }
         val dialog = Dialog(requireContext())
         dialog.setContentView(dialogView)
         dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        var routes = planViewModel.routeList.value!!
+        var details = routes[curPos].routeDetailList!!
+        if(flag == 2){
+            if(placeId == 0){
+                dialogView.findViewById<TextView>(R.id.memo_tv_content).text = routes[curPos].memo
+            }
+            if(placeId > 0){
+                var memos = ""
+                for(i in details.indices){
+                    if (details[i].placeId == placeId) {
+                        memos = details[i].memo
+                    }
+                }
+                dialogView.findViewById<TextView>(R.id.memo_tv_content).text = memos.toString()
+            }
+        }
 
-        dialogView.findViewById<Button>(R.id.memo_btn_cancle).setOnClickListener {
+        dialog.show()
+
+        dialogView.findViewById<Button>(R.id.memo_btn_cancel).setOnClickListener {
             dialog.dismiss()
         }
-        dialogView.findViewById<Button>(R.id.memo_btn_insert).setOnClickListener {
-            //추가
-            val memo = dialogView.findViewById<TextView>(R.id.memo_tv_content).text.toString()
-//            RouteService().updateRoute()
-            if(placeId == 0){
-                //day별 메모추가
-                planViewModel.routeList.observe(viewLifecycleOwner, {
+        dialogView.findViewById<AppCompatButton>(R.id.memo_btn_ok).setOnClickListener {
+            if(flag == 1){
+                val memo = dialogView.findViewById<TextView>(R.id.memo_tv_content).text.toString()
+
+                if(placeId == 0){
                     var route = Route(
-                        day = it[curPos].day,
-                        id = it[curPos].id,
-                        isCustom = it[curPos].isCustom,
+                        day = routes[curPos].day,
+                        id = routes[curPos].id,
+                        isCustom = routes[curPos].isCustom,
                         memo = memo,
-                        name = it[curPos].name,
-                        routeDetailList = it[curPos].routeDetailList,
+                        name = routes[curPos].name,
+                        routeDetailList = routes[curPos].routeDetailList,
                     )
-                    RouteService().updateRoute(route, object:RetrofitCallback<Boolean> {
+                    updateMemo(route,1)
+                    dialog.dismiss()
+                }
+                if(placeId > 0){
+                    var routeDetail = RouteDetail()
+                    for(i in details.indices){
+                        if (details[i].placeId == placeId) {
+                            routeDetail = RouteDetail(
+                                id= details[i].id,
+                                memo = memo,
+                                priority = details[i].priority
+                            )
+                        }
+                    }
+                    RouteDetailService().updateRouteDetail(routeDetail, object:RetrofitCallback<Boolean> {
                         override fun onError(t: Throwable) {
                             Log.d(TAG, "onError: ")
                         }
 
                         override fun onSuccess(code: Int, responseData: Boolean) {
-                            showCustomToast("메모가 추가되었습니다.")
                             dialog.dismiss()
                             runBlocking {
                                 planViewModel.getPlanById(planId)
@@ -400,16 +444,26 @@ class TravelPlanFragment : BaseFragment<FragmentTravelPlanBinding>(FragmentTrave
                         }
 
                     })
-                })
+                }
             }
-            if(placeId > 0){
-                Log.d(TAG, "insertMemo: ${placeId}")
-                var routeDetail = RouteDetail()
-                planViewModel.routeList.observe(viewLifecycleOwner, {
-                    val details = it[curPos].routeDetailList
-                    Log.d(TAG, "onClick: ${details}")
-                    for(i in 0.. details.size-1){
-                        Log.d(TAG, "onClick: ${placeId} ||${details[i].placeId}")
+            if(flag == 2){
+                val memo = dialogView.findViewById<TextView>(R.id.memo_tv_content).text.toString()
+                if(placeId == 0){
+                    var route = Route(
+                        day = routes[curPos].day,
+                        id = routes[curPos].id,
+                        isCustom = routes[curPos].isCustom,
+                        memo = memo,
+                        name = routes[curPos].name,
+                        routeDetailList = routes[curPos].routeDetailList,
+                    )
+                    updateMemo(route,2)
+                    dialog.dismiss()
+                }
+                //place 별 메모수정
+                if(placeId > 0){
+                    var routeDetail = RouteDetail()
+                    for(i in details.indices){
                         if (details[i].placeId == placeId) {
                             routeDetail = RouteDetail(
                                 id= details[i].id,
@@ -418,27 +472,27 @@ class TravelPlanFragment : BaseFragment<FragmentTravelPlanBinding>(FragmentTrave
                             )
                         }
                     }
-                })
-                RouteDetailService().updateRouteDetail(routeDetail, object:RetrofitCallback<Boolean> {
-                    override fun onError(t: Throwable) {
-                        Log.d(TAG, "onError: ")
-                    }
-
-                    override fun onSuccess(code: Int, responseData: Boolean) {
-                        dialog.dismiss()
-                        runBlocking {
-                            planViewModel.getPlanById(planId)
+                    RouteDetailService().updateRouteDetail(routeDetail, object:RetrofitCallback<Boolean> {
+                        override fun onError(t: Throwable) {
+                            Log.d(TAG, "onError: ")
                         }
-                    }
 
-                    override fun onFailure(code: Int) {
-                        Log.d(TAG, "onFailure: ")
-                    }
+                        override fun onSuccess(code: Int, responseData: Boolean) {
+                            dialog.dismiss()
+                            runBlocking {
+                                planViewModel.getPlanById(planId)
+                            }
+                        }
 
-                })
+                        override fun onFailure(code: Int) {
+                            Log.d(TAG, "onFailure: ")
+                        }
+
+                    })
+                }
             }
         }
-        dialog.show()
+
     }
     fun showRouteSelectDialog(){
         routeRecomDialogAdapter = RouteRecomDialogAdapter(requireContext())
