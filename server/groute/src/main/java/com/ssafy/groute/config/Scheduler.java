be9ab424dff51d6f56fa668ab5bae594069265f4
jiwoo <63338678+dino889@users.noreply.github.com>
2022-02-14 16:52:20 +0900

    #S06P12D109-312 / complete - dday fcm
    #S06P12D109-313 / fix - ended travel user fcm cron time(seoul -> utc)

--- server/groute/src/main/java/com/ssafy/groute/config/Scheduler.java
+++ server/groute/src/main/java/com/ssafy/groute/config/Scheduler.java
@@ -21,18 +21,49 @@ public class Scheduler {
     FirebaseCloudMessageService fcmService;
 
     /**
+     * utc time 05:00 == seoul 14:00
      * 매일 14시 정각에 전 날 여행 일정이 종료된 사용자에게 여행 공유 & 리뷰 작성 요청
      */
-    @Scheduled(cron = "0 0 14 * * *")
-//    @Scheduled(cron = "0 */1 * * * *")    // 1분 마다
+    @Scheduled(cron = "0 0 5 * * *")
     public void endedPlanYesterdayScheduler() throws Exception {
         List<User> userList = userService.selectEndedPlanYesterday();
 
         for (User tmp : userList) {
             if (tmp != null) {
-                fcmService.sendMessageTo(tmp.getToken(), "User", "아이엠그루트 속에서 여행은 즐거우셨나요?●'◡'●\n" + tmp.getNickname() + "님의 여행을 공유해보세요!", "UserPlanFragment");
+                fcmService.sendMessageTo(tmp.getToken(), "User", "아이엠그루트 속에서 여행은 즐거우셨나요?●'◡'●\n" + tmp.getNickname() + "님의 소중한 추억을 공유해보세요!", "UserPlanFragment");
             }
         }
 
     }
+
+    /**
+     * utc time 9:00 == seoul 18:00
+     * 매일 18시 정각에 여행 일정이 1일, 3일, 7일 남은 사용자에게 알림 전송
+     */
+    @Scheduled(cron = "0 0 9 * * *")
+    public void dayBeforeTravel() throws Exception {
+
+        List<User> oneDayBeforeUser = userService.selectOneDayBeforeTravel();
+        List<User> threeDayBeforeUser = userService.selectThreeDayBeforeTravel();
+        List<User> weekBeforeUser = userService.selectWeekBeforeTravel();
+
+        for (User tmp : oneDayBeforeUser) {
+            if (tmp != null) {
+                fcmService.sendMessageTo(tmp.getToken(), "User", "여행 1일 전입니다! 준비물은 다 챙기셨나요?\n" + tmp.getNickname() + " 님, 안전하고 즐거운 여행 다녀오세요( ღ'ᴗ'ღ )", "UserPlanFragment");
+            }
+        }
+
+        for (User tmp : threeDayBeforeUser) {
+            if (tmp != null) {
+                fcmService.sendMessageTo(tmp.getToken(), "User", "여행 3일 전입니다!\n" + tmp.getNickname() + " 님, 여행 일정에서 빠트리신 장소는 없는지 확인해 보세요!", "UserPlanFragment");
+            }
+        }
+
+        for (User tmp : weekBeforeUser) {
+            if (tmp != null) {
+                fcmService.sendMessageTo(tmp.getToken(), "User", tmp.getNickname() + " 님! 혹시 잊으신건 없으신가요?\n저와 여행떠나기 일주일 전이에요!", "UserPlanFragment");
+            }
+        }
+    }
+
 }
